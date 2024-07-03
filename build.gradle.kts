26442f9955f9645a714f5c4ca0025c32e05f08b5
Emirhan EMMEZ <emirhanemmezdev@gmail.com>
2024-07-03 23:33:54 +0300

    add application.conf file and use EngineMain

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ group = "com.emirhanemmez"
 version = "0.0.1"
 
 application {
-    mainClass.set("com.emirhanemmez.ApplicationKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
